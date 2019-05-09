08f50e6b0e402bea042e8440712aa6f3bebfebab
ShotaKiko <Shota.Kikozashvili@gmail.com>
2019-05-08 22:14:38 -0500

    Added post request endpoint

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const express = require('express');
 const server = express();
 const db = require('./data/db.js')
 const { find } = db
+const { insert } = db
+
+//middleware
+server.use(express.json())
 
 server.listen(6492, () =>{
     console.log("Listening on port 6492.")
@@ -12,6 +16,20 @@ server.listen(6492, () =>{
 
 server.get('/' , (req, res) =>{
     res.send('<h2>Hello World!<h2>')
+})
+
+//POST request endpoint~~~~~~~~~~~~~~~~~~~~~`
+
+server.post('/users' , (req , res) => {
+    const newUser = req.body;
+    
+    insert(newUser)
+    .then(addedUser => {
+        res.status(201).json(addedUser)
+    })
+    .catch(err => {
+        res.status(400).send(err, "Please provide name and bio for the user.")
+    });
 })
 
 
