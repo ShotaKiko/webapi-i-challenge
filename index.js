5cd3c851c63465fa57ec5fc3361494421e966425
ShotaKiko <Shota.Kikozashvili@gmail.com>
2019-05-08 21:36:10 -0500

    Added get request endpoint for all users

--- index.js
+++ index.js
@@ -3,3 +3,26 @@
 const express = require('express');
 
 const server = express();
+const db = require('./data/db.js')
+const { find } = db
+
+server.listen(6492, () =>{
+    console.log("Listening on port 6492.")
+})
+
+server.get('/' , (req, res) =>{
+    res.send('<h2>Hello World!<h2>')
+})
+
+
+//Get request~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+server.get('/users', (req, res) => {
+    find()
+    .then(allUsers => {
+        res.json(allUsers)
+    })
+    .catch(err => {
+        res.status(500).send(err, "The users' information could not be retireved.")
+    })
+})
